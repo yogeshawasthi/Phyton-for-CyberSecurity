dc4b80dbc7aea20b7da319219193c4b28f4bcd2b
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-11-22 20:11:20 +0545

    tommorw is mhy presentation on the topic detection and eradication of the cyber attack

--- eradication.py
+++ eradication.py
@@ -3,6 +3,3 @@ def remove_malware():
     print("Malware detected and removed successfully.")
 
 remove_malware()
-# no class today coz there was game and i need to finish aws cloud foundagtions
-# no internet br
-# Day 2 of 
